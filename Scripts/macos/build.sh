4158aae7f5908afb78dfa3aec5cb2b35095321b7
jd-13 <jackd8989@gmail.com>
2024-06-07 21:34:02 +0100

    Attempt CI build

---
+++ Scripts/macos/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+
+echo "=== Downloading JUCE ==="
+JUCE_VERSION=7.0.9
+wget https://github.com/juce-framework/JUCE/releases/download/$JUCE_VERSION/juce-$JUCE_VERSION-osx.zip \
+     -P $SCRIPT_DIR/../..
+unzip $SCRIPT_DIR/../../juce-$JUCE_VERSION-osx.zip
+
+echo "=== Setting module paths ==="
+PROJECT_DIR=$SCRIPT_DIR/../..
+sed -i '' -e 's#../../../../../../../SDKs/JUCE/modules#JUCE/modules#g' $PROJECT_DIR/KlangFalter.jucer
+
+echo "=== Generating project ==="
+JUCE_HOME=$SCRIPT_DIR/../../JUCE
+$JUCE_HOME/Projucer.app/Contents/MacOS/Projucer --resave $PROJECT_DIR/KlangFalter.jucer
+
+echo "=== Starting KlangFalter build ==="
+cd $PROJECT_DIR/Builds/MacOSX
+xcodebuild -version
+xcodebuild -project KlangFalter.xcodeproj -scheme "KlangFalter - All" -configuration Release
+
+echo "=== Collecting artefacts ==="
+mkdir -p $SCRIPT_DIR/dist
+cp -r ~/Library/Audio/Plug-Ins/VST3/KlangFalter.vst3 $SCRIPT_DIR/dist
+cp -r ~/Library/Audio/Plug-Ins/Components/KlangFalter.component $SCRIPT_DIR/dist
---
+++ Scripts/windows/build.bat
@@ -0,0 +1,23 @@
+set SCRIPT_DIR=%~dp0
+
+ECHO "=== Downloading JUCE ==="
+set JUCE_VERSION=7.0.9
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://github.com/juce-framework/JUCE/releases/download/%JUCE_VERSION%/juce-%JUCE_VERSION%-windows.zip -OutFile %SCRIPT_DIR%..\..\juce-%JUCE_VERSION%-windows.zip}"
+powershell -Command "& {Expand-Archive -LiteralPath %SCRIPT_DIR%..\..\juce-%JUCE_VERSION%-windows.zip -DestinationPath %SCRIPT_DIR%..\..}"
+
+ECHO "=== Generating project ==="
+set JUCE_HOME=%SCRIPT_DIR%..\..\JUCE
+set PROJECT_DIR=%SCRIPT_DIR%..\..
+%JUCE_HOME%\Projucer --resave %PROJECT_DIR%\KlangFalter.jucer
+
+ECHO "=== Starting KlangFalter build ==="
+cd %PROJECT_DIR%\Builds\VisualStudio2019
+msbuild -version
+msbuild KlangFalter.sln /p:Configuration=Release /p:Platform=x64 || exit /b
+
+ECHO "=== Make Release folder ==="
+cd %SCRIPT_DIR%..\..\
+mkdir -p Release
+cd Release
+
+copy %PROJECT_DIR%\Builds\VisualStudio2019\x64\Release\VST3\KlangFalter.vst3\Contents\x86_64-win\KlangFalter.vst3 || exit /b
