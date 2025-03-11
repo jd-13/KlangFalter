80a27c26b97ae9e1e5003e4099199067e3ceeabb
jd-13 <jackd8989@gmail.com>
2025-03-11 19:06:09 +0000

    Attempt separate builds

--- Scripts/macos/build.sh
+++ Scripts/macos/build.sh
@@ -10,27 +10,31 @@ wget https://github.com/juce-framework/JUCE/releases/download/$JUCE_VERSION/juce
      -P $SCRIPT_DIR/../..
 unzip $SCRIPT_DIR/../../juce-$JUCE_VERSION-osx.zip
 
-echo "=== Setting module paths ==="
-PROJECT_DIR=$SCRIPT_DIR/../..
-sed -i '' -e 's#../../../SDKs/JUCE/modules#JUCE/modules#g' $PROJECT_DIR/KlangFalter.jucer
-
-echo "=== Generating project ==="
+echo "=== Generating projects ==="
 JUCE_HOME=$SCRIPT_DIR/../../JUCE
-$JUCE_HOME/Projucer.app/Contents/MacOS/Projucer --resave $PROJECT_DIR/KlangFalter.jucer
+$JUCE_HOME/Projucer.app/Contents/MacOS/Projucer --resave $PROJECT_DIR/Projects/Intro/BodyandSoulIntro.jucer
+$JUCE_HOME/Projucer.app/Contents/MacOS/Projucer --resave $PROJECT_DIR/Projects/Body/BodyandSoulBody.jucer
+$JUCE_HOME/Projucer.app/Contents/MacOS/Projucer --resave $PROJECT_DIR/Projects/Soul/BodyandSoulSoul.jucer
+$JUCE_HOME/Projucer.app/Contents/MacOS/Projucer --resave $PROJECT_DIR/Projects/FX/BodyandSoulFX.jucer
 
-echo "=== Starting KlangFalter build ==="
-cd $PROJECT_DIR/Builds/MacOSX
+echo "=== Starting builds ==="
 xcodebuild -version
-xcodebuild -project "Body and Soul Intro.xcodeproj" -scheme "Body and Soul Intro - All" -configuration Release
+xcodebuild -project "$PROJECT_DIR/Projects/Intro/Builds/MacOSX/Body and Soul Intro.xcodeproj" -scheme "Body and Soul Intro - All" -configuration Release
+xcodebuild -project "$PROJECT_DIR/Projects/Body/Builds/MacOSX/Body and Soul Body.xcodeproj" -scheme "Body and Soul Body - All" -configuration Release
+xcodebuild -project "$PROJECT_DIR/Projects/Soul/Builds/MacOSX/Body and Soul Soul.xcodeproj" -scheme "Body and Soul Soul - All" -configuration Release
+xcodebuild -project "$PROJECT_DIR/Projects/FX/Builds/MacOSX/Body and Soul FX.xcodeproj" -scheme "Body and Soul FX - All" -configuration Release
 
 echo "=== Collecting artefacts ==="
-mkdir -p $SCRIPT_DIR/dist
+mkdir -p $SCRIPT_DIR/dist/BodyandSoulIntro
+cp -r ~/Library/Audio/Plug-Ins/VST3/TSoM-BodyandSoulIntro.vst3 $SCRIPT_DIR/dist/BodyandSoulIntro
 
-cp -r ~/Library/Audio/Plug-Ins/VST3/TSoM-BodyandSoulIntro.vst3 $SCRIPT_DIR/dist
-cp -r ~/Library/Audio/Plug-Ins/Components/TSoM-BodyandSoulIntro.component $SCRIPT_DIR/dist
+mkdir -p $SCRIPT_DIR/dist/BodyandSoul
+cp -r ~/Library/Audio/Plug-Ins/VST3/TSoM-BodyandSoulBody.vst3 $SCRIPT_DIR/dist/BodyandSoul
+cp -r ~/Library/Audio/Plug-Ins/VST3/TSoM-BodyandSoulSoul.vst3 $SCRIPT_DIR/dist/BodyandSoul
+cp -r ~/Library/Audio/Plug-Ins/VST3/TSoM-BodyandSoulFX.vst3 $SCRIPT_DIR/dist/BodyandSoul
 
-echo "=== Copying IRs ==="
-mkdir -p $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.vst3/Contents/Resources/IRs
-cp -r $PROJECT_DIR/IRs/* $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.vst3/Contents/Resources/IRs
-mkdir -p $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.component/Contents/Resources/IRs
-cp -r $PROJECT_DIR/IRs/* $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.component/Contents/Resources/IRs
+# echo "=== Copying IRs ==="
+# mkdir -p $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.vst3/Contents/Resources/IRs
+# cp -r $PROJECT_DIR/IRs/* $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.vst3/Contents/Resources/IRs
+# mkdir -p $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.component/Contents/Resources/IRs
+# cp -r $PROJECT_DIR/IRs/* $SCRIPT_DIR/dist/TSoM-BodyandSoulIntro.component/Contents/Resources/IRs
--- Scripts/windows/build.bat
+++ Scripts/windows/build.bat
@@ -5,26 +5,45 @@ set JUCE_VERSION=8.0.4
 powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://github.com/juce-framework/JUCE/releases/download/%JUCE_VERSION%/juce-%JUCE_VERSION%-windows.zip -OutFile %SCRIPT_DIR%..\..\juce-%JUCE_VERSION%-windows.zip}"
 powershell -Command "& {Expand-Archive -LiteralPath %SCRIPT_DIR%..\..\juce-%JUCE_VERSION%-windows.zip -DestinationPath %SCRIPT_DIR%..\..}"
 
-ECHO "=== Generating project ==="
+ECHO "=== Generating projects ==="
 set JUCE_HOME=%SCRIPT_DIR%..\..\JUCE
 set PROJECT_DIR=%SCRIPT_DIR%..\..
-%JUCE_HOME%\Projucer --resave %PROJECT_DIR%\KlangFalter.jucer
+%JUCE_HOME%\Projucer --resave %PROJECT_DIR%\Projects\Intro\BodyandSoulIntro.jucer
+%JUCE_HOME%\Projucer --resave %PROJECT_DIR%\Projects\Body\BodyandSoulBody.jucer
+%JUCE_HOME%\Projucer --resave %PROJECT_DIR%\Projects\Soul\BodyandSoulSoul.jucer
+%JUCE_HOME%\Projucer --resave %PROJECT_DIR%\Projects\FX\BodyandSoulFX.jucer
 
-ECHO "=== Starting KlangFalter build ==="
-cd %PROJECT_DIR%\Builds\VisualStudio2019
+ECHO "=== Starting builds ==="
 msbuild -version
+cd %PROJECT_DIR%\Projects\Intro\Builds\VisualStudio2019
 msbuild "Body and Soul Intro.sln" /p:Configuration=Release /p:Platform=x64 || exit /b
+cd %PROJECT_DIR%\Projects\Body\Builds\VisualStudio2019
+msbuild "Body and Soul Body.sln" /p:Configuration=Release /p:Platform=x64 || exit /b
+cd %PROJECT_DIR%\Projects\Soul\Builds\VisualStudio2019
+msbuild "Body and Soul Soul.sln" /p:Configuration=Release /p:Platform=x64 || exit /b
+cd %PROJECT_DIR%\Projects\FX\Builds\VisualStudio2019
+msbuild "Body and Soul FX.sln" /p:Configuration=Release /p:Platform=x64 || exit /b
 
-ECHO "=== Make Release folder ==="
+ECHO "=== Making Release folder ==="
 cd %SCRIPT_DIR%..\..\
-mkdir -p Release\TSoM-BodyandSoulIntro.vst3\Contents
-cd Release\TSoM-BodyandSoulIntro.vst3\Contents
+mkdir -p Release\TSoM-BodyandSoulIntro.vst3\Contents\x86_64-win
+mkdir -p Release\TSoM-BodyandSoulBody.vst3\Contents\x86_64-win
+mkdir -p Release\TSoM-BodyandSoulSoul.vst3\Contents\x86_64-win
+mkdir -p Release\TSoM-BodyandSoulFX.vst3\Contents\x86_64-win
 
-mkdir x86_64-win
-cd x86_64-win
-copy %PROJECT_DIR%\Builds\VisualStudio2019\x64\Release\VST3\TSoM-BodyandSoulIntro.vst3\Contents\x86_64-win\TSoM-BodyandSoulIntro.vst3 || exit /b
+cd Release\TSoM-BodyandSoulIntro.vst3\Contents\x86_64-win
+copy %PROJECT_DIR%\Projects\Intro\Builds\VisualStudio2019\x64\Release\VST3\TSoM-BodyandSoulIntro.vst3\Contents\x86_64-win\TSoM-BodyandSoulIntro.vst3 || exit /b
 
-ECHO "=== Copying IRs ==="
-cd ..
-mkdir -p Resources\IRs
-robocopy %PROJECT_DIR%\IRs\ Resources\IRs\ /e /s
+cd Release\TSoM-BodyandSoulBody.vst3\Contents\x86_64-win
+copy %PROJECT_DIR%\Projects\Body\Builds\VisualStudio2019\x64\Release\VST3\TSoM-BodyandSoulBody.vst3\Contents\x86_64-win\TSoM-BodyandSoulBody.vst3 || exit /b
+
+cd Release\TSoM-BodyandSoulSoul.vst3\Contents\x86_64-win
+copy %PROJECT_DIR%\Projects\Soul\Builds\VisualStudio2019\x64\Release\VST3\TSoM-BodyandSoulSoul.vst3\Contents\x86_64-win\TSoM-BodyandSoulSoul.vst3 || exit /b
+
+cd Release\TSoM-BodyandSoulFX.vst3\Contents\x86_64-win
+copy %PROJECT_DIR%\Projects\FX\Builds\VisualStudio2019\x64\Release\VST3\TSoM-BodyandSoulFX.vst3\Contents\x86_64-win\TSoM-BodyandSoulFX.vst3 || exit /b
+
+@REM ECHO "=== Copying IRs ==="
+@REM cd ..
+@REM mkdir -p Resources\IRs
+@REM robocopy %PROJECT_DIR%\IRs\ Resources\IRs\ /e /s
